59052bba01371def0fa1c1de29397ca1716ef9a7
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-02-12 12:45:56 +0500

    Basic react knowledge complete. Start project

--- mi-store-clone/src/App.js
+++ mi-store-clone/src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
+// Importing the new component created 
+// This will work even if we write "import dfsdf from './Navbar'" because of the "export default Navbar" line at the end of the Navbar.js file 
+import Navbar from './Navbar'; 
 
 function App() {
-  // We can have only one tag in return. Solution for this is to wrap everything in an empty tag.
 
   // for inline css
   const stylingoption = {
     backgroundColor: "red"
   }
 
-  return (
+  return (   // We can have only one tag in return. Solution for this is to wrap everything in an empty tag.
     // This div is not HTML. It is "jsx", which is a combo of html and js
     // We can apply CSS from the App.css file being imported.
     // We can also apply inline css like shown below
-    <div style={stylingoption}>
-      hello
-    </div>
+    <>
+      <Navbar />
+      <div style={stylingoption}>
+        hello
+      </div>
+    </>
   );
 }
 
---
+++ mi-store-clone/src/Navbar.js
@@ -0,0 +1,12 @@
+// This js boiler template is made by "rafce" 
+// the "e" at the end of "rafce" gives us the last line of "export default Navbar"
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
