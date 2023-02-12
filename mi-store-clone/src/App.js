7342b5ac627f36bda868bc41c5c5074282a2e1fa
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-02-12 12:47:40 +0500

    Clean App.js file. Delete Navbar.js file. Start project

--- mi-store-clone/src/App.js
+++ mi-store-clone/src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
-// Importing the new component created 
-// This will work even if we write "import dfsdf from './Navbar'" because of the "export default Navbar" line at the end of the Navbar.js file 
-import Navbar from './Navbar'; 
 
 function App() {
-
-  // for inline css
-  const stylingoption = {
-    backgroundColor: "red"
-  }
-
-  return (   // We can have only one tag in return. Solution for this is to wrap everything in an empty tag.
-    // This div is not HTML. It is "jsx", which is a combo of html and js
-    // We can apply CSS from the App.css file being imported.
-    // We can also apply inline css like shown below
-    <>
-      <Navbar />
-      <div style={stylingoption}>
-        hello
+  return ( 
+      <div>
+        
       </div>
-    </>
   );
 }
 
