3f34b9051f89561b32ba697cb35c68b1761ec347
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-02-12 12:32:35 +0500

    Learn CSS in jsx

--- mi-store-clone/src/App.js
+++ mi-store-clone/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 
 function App() {
+  // We can have only one tag in return. Solution for this is to wrap everything in an empty tag.
+
+  // for inline css
+  const stylingoption = {
+    backgroundColor: "red"
+  }
+
   return (
-    <div>
+    // This div is not HTML. It is "jsx", which is a combo of html and js
+    // We can apply CSS from the App.css file being imported.
+    // We can also apply inline css like shown below
+    <div style={stylingoption}>
       hello
     </div>
   );
