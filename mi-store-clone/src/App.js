16d7a020a59c7d2116c42359a74f14f24df222ef
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-02-13 15:57:44 +0500

    Complete overall HOT ACCESSORIES menu

--- mi-store-clone/src/App.js
+++ mi-store-clone/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar.js';
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router,Route, Routes } from "react-router-dom"
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
 import React from 'react';
@@ -9,6 +9,7 @@ import Offers from "./components/Offers.js"
 import Heading from "./components/Heading.js"
 import StarProduct from "./components/StarProduct.js"
 import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.js"
+import HotAccessories from "./components/HotAccessories.js"
 
 
 function App() {
@@ -23,6 +24,26 @@ function App() {
         <Heading text="HOT ACCESSORIES"/>
 
         <HotAccessoriesMenu />
+
+        <Routes>
+          <Route exact path="/music" element={<HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />}>
+          </Route>
+          <Route exact path="/smartDevice" element={<HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}  />}>
+          </Route>
+
+          <Route exact path="/home" element={<HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />}>
+          </Route>
+
+          <Route exact path="/lifestyle" element={<HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />}>
+          </Route>
+
+
+          <Route exact path="/mobileAccessories" element={<HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}  />}>
+          </Route>
+        </Routes>
+
+        
+
 
       </Router>
   );
